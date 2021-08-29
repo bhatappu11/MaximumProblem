25e49f5bca9b8b8c810c12327c5918f429951344
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-29 22:53:52 +0530

    ADDED: method to get maximum of three floats

--- MaximumProblem/src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ MaximumProblem/src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -5,6 +5,8 @@ public class MaximumProblem {
 		if(firstInteger.compareTo(secondInteger)>0) {
 			if(firstInteger.compareTo(thirdInteger)>0)
 				System.out.println("Maximum number: "+firstInteger);
+			else
+				System.out.println("Maximum number: "+secondInteger);
 		}
 		else {
 			if(secondInteger.compareTo(thirdInteger)>0)
@@ -13,8 +15,23 @@ public class MaximumProblem {
 				System.out.println("Maximum number: "+thirdInteger);
 		}
 	}
+	public static void getMaximumNumber(Float firstFloat,Float secondFloat,Float thirdFloat) {
+		if(firstFloat.compareTo(secondFloat)>0) {
+			if(firstFloat.compareTo(thirdFloat)>0)
+				System.out.println("Maximum number: "+firstFloat);
+			else
+				System.out.println("Maximum number: "+thirdFloat);
+		}
+		else {
+			if(secondFloat.compareTo(thirdFloat)>0)
+				System.out.println("Maximum number: "+secondFloat);
+			else
+				System.out.println("Maximum number: "+thirdFloat);
+		}
+	}
 	public static void main(String[] args) {
-		getMaximumNumber(4,7,2);		
+		getMaximumNumber(4,7,2);
+		getMaximumNumber(2.5f,1.2f,5.7f);
 	}
 
 }
