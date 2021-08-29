21035b09d72ecd688c24ca957f0707d1200dd6dc
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-29 22:27:38 +0530

    ADDED: Method to get maximum of three integers

--- MaximumProblem/src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ MaximumProblem/src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem {
-
+	public static void getMaximumNumber(Integer firstInteger,Integer secondInteger,Integer thirdInteger) {
+		if(firstInteger.compareTo(secondInteger)>0) {
+			if(firstInteger.compareTo(thirdInteger)>0)
+				System.out.println("Maximum number: "+firstInteger);
+		}
+		else {
+			if(secondInteger.compareTo(thirdInteger)>0)
+				System.out.println("Maximum number: "+secondInteger);
+			else
+				System.out.println("Maximum number: "+thirdInteger);
+		}
+	}
 	public static void main(String[] args) {
-
+		getMaximumNumber(4,7,2);		
 	}
 
 }
